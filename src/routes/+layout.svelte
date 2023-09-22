2687a0455552e2b15afe101cdc0d10c3ba62f8bf
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-22 21:07:36 +0200

    Initializing svelte-icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
 	import "../app.css";
 </script>
 
-<slot />
+<div class="bg-gray-900 h-full min-h-screen ">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>WelcomeasiydoiUADSHK to SvelteKit</h1>
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<iconify-icon icon="eva:people-outline"></iconify-icon>
+<iconify-icon inline icon="material-symbols:account-circle"></iconify-icon>
