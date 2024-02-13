7b415799c93836950bb71f11336116d5d342202e
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-02-13 17:52:24 +0200

    chore: refactor

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
-	import Icon from "./Icon.svelte";
+  import Icon from "./Icon.svelte";
 
-	export let item : any
+  export let item: any;
 </script>
 
+<div class="flex gap-3 items-start h-auto">
+  <div class="flex">
+    <div class="flex flex-col items-center">
+      <div class={`w-2.5 h-2.5 rounded-full ${item.index === 0 ? "bg-secondary-100" : "bg-primary-dark-300"}`} />
+      <div class="w-[2px] h-full flex-1 bg-primary-dark-300"></div>
+    </div>
+    <div class="space-y-2 -mt-[6px] px-8 pb-10">
+      <h1 class={`font-bold text-2xl ${item.index === 0 && "text-secondary-100"}`}>{item.title}</h1>
+      <h2 class="text-blue-600 text-sm">{item.time}</h2>
 
-<div class="flex gap-4 items-start mb-2 h-auto">
-	<div class="flex flex-col h-full flex-grow flex-shrink items-center justify-start gap-2 mt-1.5">
-		<div class={`w-2.5 h-2.5 rounded-full self-start ${item.index === 0 ? 'bg-secondary-100' : 'bg-primary-dark-300 '}`} />
-		<div class="w-[2px] min-h-[150px] 2xl:min-h-[120px] bg-primary-dark-300" />
-	</div>
-	<div class="mb-10">
-		<h1 class={`font-semibold ${item.index === 0 && 'text-secondary-100'}`}>{item.title}</h1>
-		<div>
-			<h2 class="text-blue-600 text-sm">{item.time}</h2>
-			<div class="flex items-center">
-				<Icon
-					classAttr="text-gray-500 w-3 h-3"
-					svgData="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-				/>
-				<p class="text-[13px] text-gray-500">
-					{item.location}
-				</p>
-			</div>
-			<p class="text-sm leading-relaxed text-gray-300">
-				{item.description}
-			</p>
-		</div>
-	</div>
+      <p class="leading-relaxed">
+        {item.description}
+      </p>
+    </div>
+  </div>
 </div>
---
+++ src/providers/Transition.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+
+  export let url: URL;
+</script>
+
+{#key url}
+  <div in:fly={{ y: -50, duration: 400 }}>
+    <slot />
+  </div>
+{/key}
